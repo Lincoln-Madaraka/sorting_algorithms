38a3a20e2e09c2215a0e384b4cd4421c44e9c60e
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2024-04-30 21:53:48 +0300

    my commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort a list of ints using bubble sort
+ * @array: Array of integers
+ * @size: Size of the arrays
+ * Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, stat;
+
+	if (size < 2)
+	{
+		return;
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		stat = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+				stat = 1;
+			}
+		}
+		if (stat == 0)
+		{
+			break;
+		}
+	}
+}
+
+/**
+ * swap - Swap Two integer
+ * @num_1: integer 1
+ * @num_2: integer 2
+ *
+ * Return: Nothing
+ */
+void swap(int *num_1, int *num_2)
+{
+	int tmp;
+
+	tmp = *num_1;
+	*num_1 = *num_2;
+	*num_2 = tmp;
+}
